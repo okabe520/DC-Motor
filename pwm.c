e31d0fe89d71e37f251d13e11b259808fd1b971d
zsl425 <2938988480@qq.com>
2024-11-19 22:00:06 +0800

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -1,6 +1,8 @@
 #include "pwm.h"
 #include "led.h"
 #include "usart.h"
+ 
+
 
 void TIM1_PWM_Init(u32 arr, u32 psc)
 {
@@ -55,3 +57,57 @@ void TIM1_PWM_Init(u32 arr, u32 psc)
     // 启动TIM1
     TIM_Cmd(TIM1, ENABLE);
 	}
+
+	void TIM8_PWM_Init(u32 arr, u32 psc)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+    TIM_TimeBaseInitTypeDef TIM_TimeBaseStructure;
+    TIM_OCInitTypeDef TIM_OCInitStructure;
+
+    // 使能TIM8和GPIOC、GPIOB时钟
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM8, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOB, ENABLE);
+
+    // 设置GPIOC6、GPIOB0为TIM8 CH1和CH1N模式，生成PWM输出
+    GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_TIM8); // GPIOC6复用为TIM8_CH1
+    GPIO_PinAFConfig(GPIOB, GPIO_PinSource0, GPIO_AF_TIM8); // GPIOB0复用为TIM8_CH1N
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;            // 配置GPIOC6
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;         // 复用模式
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;   // 速度100MHz
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;       // 推挽复用输出
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;         // 上拉
+    GPIO_Init(GPIOC, &GPIO_InitStructure);               // 初始化GPIOC6
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;           // 配置GPIOB0
+    GPIO_Init(GPIOB, &GPIO_InitStructure);               // 初始化GPIOB0
+
+    // 初始化TIM8定时器
+    TIM_TimeBaseStructure.TIM_Prescaler = psc;             // 定时器预分频
+    TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up; // 向上计数模式
+    TIM_TimeBaseStructure.TIM_Period = arr;                // 自动重装载值
+    TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM_TimeBaseInit(TIM8, &TIM_TimeBaseStructure);        // 初始化TIM8
+
+    // 初始化TIM8通道1的PWM模式
+    TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;      // PWM模式1
+    TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;  // 主输出使能
+    TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Enable; // 互补输出使能
+    TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;       // 主输出极性：高
+    TIM_OCInitStructure.TIM_OCNPolarity = TIM_OCNPolarity_High;      // 互补输出极性：高
+    TIM_OCInitStructure.TIM_OCIdleState = TIM_OCIdleState_Set;      // 主输出空闲状态：置位
+    TIM_OCInitStructure.TIM_OCNIdleState = TIM_OCNIdleState_Reset;  // 互补输出空闲状态：复位
+
+    TIM_OC1Init(TIM8, &TIM_OCInitStructure);               // 初始化TIM8通道1
+    TIM_OC1PreloadConfig(TIM8, TIM_OCPreload_Enable);      // 使能TIM8通道1预装载寄存器
+
+    // 使能TIM8 ARR预装载寄存器
+    TIM_ARRPreloadConfig(TIM8, ENABLE);
+
+    // 使能TIM8高级控制定时器输出
+    TIM_CtrlPWMOutputs(TIM8, ENABLE);
+
+    // 启动TIM8
+    TIM_Cmd(TIM8, ENABLE);
+}
